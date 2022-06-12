e023afee46032b090fc8a06ed77b925f4e5a2997
Jonathan Constant <jconstantdevry@yahoo.com>
2022-06-11 21:17:40 -0400

    Created Profile Model and Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.site.register(Profile)
+class ProfileAdmin(admin.AdminSite):
+    readonly_field = ('id',)
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.deconstruct import deconstructible
+
+class Generator():
+    
+    def __init__(self):
+        pass
+    
+    def __call__(self, instance, filename):
+        ext  = filename.split('.')[-1]
+        path = f'media/account/{instance.user.id}/images'
+        name = f'profile_image.{ext}'
+        return os.path.join(path, name)
+
+user_profile_path = Generator()  
+
+class Profile(models.Models):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=user_profile_path, blank=True, null=True)
+        
+
+
